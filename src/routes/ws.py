c02c8413675e2ebf7cf5a13f5b85aedb6b1a6548
Daniel <daniel.00100111@gmail.com>
2026-04-21 18:51:13 -0600

    login and websocket done

--- src/routes/ws.py
+++ src/routes/ws.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from jose import jwt, JWTError
 
 from core.config import settings
@@ -6,9 +6,28 @@ from core.config import settings
 router = APIRouter()
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
 @router.websocket("/ws")
 async def websocket_endpoint(ws: WebSocket):
-    await ws.accept()
+    await manager.connect(ws)
 
     token = ws.cookies.get("access_token")
 
@@ -21,11 +40,14 @@ async def websocket_endpoint(ws: WebSocket):
             token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
         )
         user = payload.get("sub")
-        print(ws.cookies)
     except JWTError:
         await ws.close()
         return
 
-    while True:
-        data = await ws.receive_text()
-        await ws.send_text(f"{user}: {data}")
+    try:
+        while True:
+            data = await ws.receive_text()
+            await manager.broadcast(data)
+
+    except WebSocketDisconnect:
+        manager.disconnect(ws)
